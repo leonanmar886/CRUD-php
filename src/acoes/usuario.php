e07548b9a6fd2ba621afddd637a5996b7a47d88c
leonan <leonan.marques886@gmail.com>
2022-02-22 20:32:04 -0300

    Operações CRUD no banco de dados implementadas

--- src/acoes/usuario.php
+++ src/acoes/usuario.php
@@ -6,23 +6,23 @@ function buscarUsuario($coneccao, $id){
 };
 
 function readUsuario($coneccao){
-    return readUsuarioDB($coneccao);
+    return readUsuarioBD($coneccao);
 };
 
 function updateUsuario($coneccao, $id, $nome, $email){
-    $updateUsuarioDB = updateUsuarioDB($coneccao, $id, $nome, $email);
+    $updateUsuarioDB = updateUsuarioBD($coneccao, $id, $nome, $email);
     $mensagem = $updateUsuarioDB == 1 ? 'Usuario atualizado com sucesso' : 'Falha na atualização';
     return header("Local: ./read.php?mensagem=$mensagem");
 }
 
 function createUsuario($coneccao, $nome, $email){
-    $createUsuarioDB = createUsuarioDB($coneccao, $nome, $email);
+    $createUsuarioDB = createUsuarioBD($coneccao, $nome, $email);
     $mensagem = $createUsuarioDB == 1 ? 'Usuario criado com sucesso' : 'Falha na criação';
     return header("Local: ./read.php?mensagem=$mensagem");
 }
 
 function deleteUsuario($coneccao, $id){
-    $deleteUsuarioDB = deleteUsuarioDB($coneccao, $id);
+    $deleteUsuarioDB = deleteUsuarioBD($coneccao, $id);
     $mensagem = $deleteUsuarioDB == 1 ? 'Usuario deletado com sucesso' : 'Falha em deletar o usuario';
     return header("Local: ./read.php?mensagem=$mensagem");
 }
---
+++ src/banco/usuario.php
@@ -0,0 +1,83 @@
+<?php
+
+function buscarUsuarioBD($coneccao, $id){
+    $id = mysqli_real_escape_string($coneccao, $id);
+
+    $sql = "SELECT * FROM usuarios WHERE id = ?";
+    $stmt = mysqli_stmt_init($coneccao);
+
+    if(!mysqli_stmt_prepare($stmt, $sql))
+        exit('Erro na consulta SQL');
+    
+        mysqli_stmt_bind_param($stmt, 'i', $id);
+        mysqli_stmt_execute($stmt);
+
+        $usuario = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+
+        mysqli_close($coneccao);
+        return $usuario;
+}
+
+function createUsuarioBD($coneccao, $nome, $email){
+    $nome = mysqli_real_escape_string($coneccao, $nome);
+    $email = mysqli_real_escape_string($coneccao, $email);
+
+    if($nome && $email){
+        $sql = "INSERT INTO usuarios (nome, email) values (?, ?)";
+        $stmt = mysqli_stmt_init($coneccao);
+
+        if(!mysqli_stmt_prepare($stmt, $sql))
+            exit('Erro na consulta SQL');
+
+        mysqli_stmt_bind_param($stmt, 'ss', $nome, $email);
+        mysqli_stmt_execute($stmt);
+        mysqli_close($coneccao);
+        return true;
+    }
+}
+
+function readUsuarioBD($coneccao){
+    $usuarios = [];
+
+    $sql = "SELECT * FROM usuarios";
+    $resultado = mysqli_query($coneccao, $sql);
+
+    $quantidadeRegistros = mysqli_num_rows($resultado);
+
+    if ($quantidadeRegistros > 0) 
+        mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+    
+    mysqli_close($coneccao);
+    return $usuarios;
+}
+
+function updateUsuarioBD($coneccao, $id, $nome, $email){
+    if($id && $nome && $email){
+        $sql = "UPDATE usuarios SET nome = ?, email = ? WHERE id = ?";
+        $stmt = mysqli_stmt_init($coneccao);
+
+        if(!mysqli_stmt_prepare($stmt, $sql))
+            exit('Erro na consulta SQL');
+
+        mysqli_stmt_bind_param($stmt, 'ssi', $nome, $email, $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_close($coneccao);
+        return true;
+    }
+}
+
+function deleteUsuarioBD($coneccao, $id){
+    $id = mysqli_real_escape_string($coneccao, $id);
+
+    if($id){
+        $sql = "DELETE FROM usuarios WHERE id = ?";
+        $stmt = mysqli_stmt_init($coneccao);
+
+        if(!mysqli_stmt_prepare($stmt, $sql))
+            exit('Erro na consulta SQL');
+
+        mysqli_stmt_bind_param($stmt, 'i', $id);
+        mysqli_stmt_execute($stmt);
+        return true;
+    }
+}
